ab91c1b6505f2e33b29e4eb720623fa46cce1937
Alex Remizov <rem.remizov@yandex.ru>
2022-01-07 19:36:45 -0500

    Add project structure

---
+++ src/main/java/com/remizov/brest/ApplicationInitializer.java
@@ -0,0 +1,14 @@
+package com.remizov.brest;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //bootstrap dispatcherservlet
+
+    }
+}
